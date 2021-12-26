57fcda9b05001c5cdf8ceaa0346d3e9028787e7b
Victor Sterea <sterea.victor@gmail.com>
2021-12-26 22:52:16 +0200

    #42 - Implementare tab alte optiuni
    Tabul contine optiunea de stergere subarbore nod curent + optiunea de resetare a arborelui

--- src/componente.h
+++ src/componente.h
@@ -60,10 +60,20 @@ GetContainerTabNod(shared_ptr<ComponentBase> input_val_nod,
                    shared_ptr<ComponentBase> butoane_salvare_copii_tab_nod);
 
 shared_ptr<ComponentBase>
+GetButonStergereSubarbore(ButtonOption &optiune_buton_stergere_subarbore);
+
+shared_ptr<ComponentBase>
+GetButonResetareArbore(ButtonOption &optiune_buton_resetare);
+
+
+shared_ptr<ComponentBase>
 GetTabEditareNodCurent(shared_ptr<ComponentBase> container_tab_nod,
                        shared_ptr<ComponentBase> input_val_nod,
                        shared_ptr<ComponentBase> buton_salvare_nod,
                        shared_ptr<ComponentBase> butoane_salvare_copii_tab_nod);
+
+shared_ptr<ComponentBase>
+GetTabAlteOptiuni(shared_ptr<ComponentBase> container_tab_alte_optiuni);
 
 shared_ptr<ComponentBase>
 GetTabOptiuniParcurgeri(shared_ptr<ComponentBase> container_meniu_parcurgeri);
@@ -79,6 +89,7 @@ RandareAdancime0(shared_ptr<ComponentBase> container_adancime_0,
                  shared_ptr<ComponentBase> tab_detalii_arbore,
                  shared_ptr<ComponentBase> tab_detalii_nod,
                  shared_ptr<ComponentBase> tab_editare_nod_crt,
+                 shared_ptr<ComponentBase> tab_alte_optiuni,
                  shared_ptr<ComponentBase> tab_reprezentare_grafica);
 
 shared_ptr<ComponentBase>
